0ceead2adb6e44c9a78f324510f35e833da9f2c8
Shawn Gotoh <zaramme@gmail.com>
2019-08-05 22:54:20 +0200

    - add function to append first raw if form is empty

--- src/components/ZForm.vue
+++ src/components/ZForm.vue
@@ -1,16 +1,38 @@
 <template>
 <div class="zform">
-  <z-form-container v-model="value.items"></z-form-container>
+  <template v-if="isFormEmpty">
+    <div class="row page">
+      <el-button icon="el-icon-plus" @click="addNewRow" round>Add first page</el-button>
+    </div>
+  </template>
+  <template v-else>
+    <z-form-container v-model="value.items"></z-form-container>
+  </template>
 </div>
 </template>
 
 <script>
 import ZFormContainer from './ZFormContainer'
+import AddableMixin from '@/mixins/AddableMixin'
 
 export default {
   name: 'ZForm',
   props: ['value'],
-  components:{ZFormContainer}
+  mixins:[AddableMixin],
+  components:{ZFormContainer},
+  methods:{
+    addNewRow(){
+      this.addChildRow({
+        type: 'page',
+        title: 'new page'
+      })
+    }
+  },
+  computed:{
+    isFormEmpty(){
+      return !Object.keys(this.value).length;
+    }
+  }
 }
 </script>
 

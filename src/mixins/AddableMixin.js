eaadbcb896ffb9114bcd15ab5c3e39f42e330402
Shawn Gotoh <zaramme@gmail.com>
2019-08-06 08:39:47 +0200

    - add detailed comments for mixin

--- src/mixins/AddableMixin.js
+++ src/mixins/AddableMixin.js
@@ -1,4 +1,4 @@
-// append manupurating-row function to like insert/delete/swap
+// append manipurating row function to component*(e.g. insert/delete/swap rows)
 import UuidGeneratableMixin from '@/mixins/UuidGeneratableMixin'
 export default {
   mixins:[UuidGeneratableMixin],
@@ -17,21 +17,21 @@ export default {
     deleteSelf(){
       // if it does NOT has child item, delete directly
       if(!this.value['items']){
-        // deligate to parent component(usually v-form-container)
+        // delegate delete process to parent component(usually v-form-container)
         this.$emit('delete', this.IndexOfRow)
         return
       }
-      // if if has child item, confirm by dialog 
+      // if if has child item, confirm by dialog before delete
       this.$confirm('This command will also delete items inside. Continue?', 'Warning', {
         confirmButtonText: 'OK',
         cancelButtonText: 'Cancel',
         type: 'warning'
       }).then(() => {
-        // deligate to parent component(usually v-form-container)
+        // delegate delete process to parent component(usually v-form-container)
         this.$emit('delete', this.IndexOfRow)
         return
       }).catch(() => {
-        // nothing to do
+        // do nothing
       })
     },
     deleteSiblingRow(indexToDelete){
@@ -39,16 +39,18 @@ export default {
     },
     swapSiblingRow(indexA, indexB){
       if(!this.value[indexA] || !this.value[indexB]){
-        return
+        return // it means specified row is on first or last row.
       }
       var tmp = this.value[indexA]
       this.$set(this.value, indexA ,this.value[indexB])
       this.$set(this.value, indexB ,tmp)
     },
     moveRowUpward(){
+      // delegate to parent component(usually v-form-container)
       this.$emit('swap', this.IndexOfRow, this.IndexOfRow-1)
     },
     moveRowDownward(){
+      // delegate swap process to parent component(usually v-form-container)
       this.$emit('swap', this.IndexOfRow, this.IndexOfRow+1)
     },
     _createNewRow(content){
@@ -59,6 +61,7 @@ export default {
       for(let key in content){
         newRow[key] = content[key]
       }
+      // TODO: error handling if data structure is iligal
       return newRow
     }
   }
--- src/mixins/EditableMixin.js
+++ src/mixins/EditableMixin.js
@@ -1,22 +1,30 @@
+// append function to manage edit state of row.
 export default {
   data () {
     return {
-      editing_data:null, // keep data in edit temprarily
+      // keep data in edit temprarily
+      editing_data:null,
+
+      // setting about which columns can be editable
+      // can be override if the component allows to edit other key(e.g. response_type)
       editable_keys:['title']
     }
   },
   methods:{
     edit(){
+      // init editing data object
       this.editing_data = {}
       for(let key of this.editable_keys){
         this.$set(this.editing_data, key,this.value[key])
       }
     },
     save(){
+      // overwrite form data by editing data per key defined by editable_keys
       for(let key of this.editable_keys){
-          console.log(key)
         this.value[key] = this.editing_data[key]
       }
+
+      // quit editing mode
       this.editing_data = null
     },
     cancelEdit(){
